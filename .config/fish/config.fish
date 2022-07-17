8e584f8fe1cb038abd55c7341e6ae6628f68db26
pedrotchang <pedrochang91@gmail.com>
2022-07-16 23:00:09 -0700

    adds current .config folder

---
+++ .config/fish/config.fish
@@ -0,0 +1,60 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    #
+end
+
+function ..
+    cd ..
+end
+
+function ..2
+    cd ..
+    cd ..
+end
+
+function ..3
+    cd ..
+    cd ..
+    cd ..
+end
+
+
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# Aliases
+
+
+# shortcuts
+alias c='clear'
+alias g='git'
+alias gbr="git branch"
+alias gst="git status"
+alias p='pwd -P'
+alias q='exit'
+alias la='ls -A'
+alias l='ls -CF'
+command -qv nvim && alias vim nvim
+
+
+set -gx EDITOR nvim
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+set -gx PATH /.emacs.d/bin $PATH
+set -gx PATH BUN_INSTALL/bin $PATH
+
+# This config an alias for my git bare repo dotfiles.
+alias config='/opt/homebrew/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$(frum init)"
+
