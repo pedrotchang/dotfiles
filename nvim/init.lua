18477807dcf01aa92f8a9d95101362eb4b6ab5ec
Pedro Chang <pedrotychang@gmail.com>
2025-01-27 19:13:14 -0800

    feat: slim version of nvim, because I am tired of it breaking

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,35 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("config.secondbrain")
+print("nvim/init.lua Reloaded!")
+
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
+-- Source Current Files
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+-- Execute Current Lin
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+-- Execute Text in Visual Mode
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Yank -> Quick Highlight
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- vim.api.nvim_create_autocmd("TermOpen", {
+--   group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+--   callback = function()
+--     vim.opt.number = false
+--     vim.opt.relativenumber = false
+--   end,
+-- })
+--
+
