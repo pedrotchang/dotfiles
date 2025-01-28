8ad01d0d9c1f83d9214018965c08d619088e06cc
Pedro Chang <pedrotychang@gmail.com>
2025-01-27 23:18:59 -0800

    feat:mvp finish

---
+++ README.md
@@ -0,0 +1,29 @@
+# Why
+My Neovim config was so bloated...and I didn't even know what was installed.
+
+In my desire to do things fast. I am going backwards.
+
+LazyVim is a Neovim setup powered by 💤 lazy.nvim to make it easy to customize and extend your config.
+
+I finally realized that [LazyVim.org](https://www.lazyvim.org/) is different than [Lazy.Folke.io](https://lazy.folke.io/).
+
+The creator of LazyVim though, is Folke. The `.org` though, is not his design.
+
+It is a larger config, with a bunch of different plugins.
+
+So many, in fact, that I have no idea *what* is being installed.
+
+## Philosophy
+
+I am had a journey for about a year being a minimalist.
+
+I read a book "Goodbye Things" by [Fumio Sasaki](https://www.goodreads.com/author/show/15267855.Fumio_Sasaki), which changed my perspective on things.
+
+It was a section where there was, not the author, someone who put all their possessions inside storage, and day by day, they took out 1 item they needed.
+
+I think that is a great way to look at things, if it is unused, it's possesing you!
+
+Therefore, with that philosophy, I decided to redo my Neovim config, and make it extremely lightweight, and lean.
+
+This is going to be a bespoke setup for myself, so if you do use it, just make note that it may not work as intended.
+
--- nvim/init.lua
+++ nvim/init.lua
@@ -2,8 +2,7 @@ require("config.lazy")
 print("nvim/init.lua Reloaded!")
 
 vim.opt.shiftwidth = 4
--- 'p' only pastes what is on the clipboard buffer
--- vim.opt.clipboard = "unnamedplus" 
+vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -24,12 +23,35 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- vim.api.nvim_create_autocmd("TermOpen", {
---   group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
---   callback = function()
---     vim.opt.number = false
---     vim.opt.relativenumber = false
---   end,
--- })
---
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
 
+local job_id = 0
+vim.keymap.set("n", "<space>to", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+
+  job_id = vim.bo.channel
+end)
+
+local current_command = ""
+vim.keymap.set("n", "<space>te", function()
+  current_command = vim.fn.input("Command: ")
+end)
+
+vim.keymap.set("n", "<space>tr", function()
+  if current_command == "" then
+    current_command = vim.fn.input("Command: ")
+  end
+
+  vim.fn.chansend(job_id, { current_command .. "\r\n" })
+end)
+
+vim.keymap.set("n", "-", "<cmd>Oil<CR>")
