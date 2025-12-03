62a7880bccf6663c483dea203eb61aaa173d9663
Pedro Chang <pedrotychang@gmail.com>
2025-12-02 22:43:23 -0800

    work: update treesitter for macos

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -3,20 +3,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      -- Install parsers
-      local ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "markdown", "markdown_inline" }
-      require("nvim-treesitter").install(ensure_installed)
-
-      -- Enable treesitter highlighting (uses vim.treesitter in Neovim 0.10+)
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function(args)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
-          if ok and stats and stats.size > max_filesize then
-            return
-          end
-          pcall(vim.treesitter.start, args.buf)
-        end,
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "markdown", "markdown_inline" },
+        highlight = { enable = true },
+        indent = { enable = true },
       })
     end,
   }
