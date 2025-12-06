011ddceea1210ef3fb0d093d3b9d2cccc460b4b6
Pedro T. Chang <pedrotychang@gmail.com>
2025-12-05 23:11:02 -0800

    work: fix nvim for linux

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -3,11 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter").setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "markdown", "markdown_inline" },
-      })
       vim.treesitter.language.register("markdown", "mdx")
     end,
-    main = "nvim-treesitter.configs",
   }
 }
